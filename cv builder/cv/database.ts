9af4bd52b57d30aaf8dfc0d2d77ab17495b5e7d3
Daniyal Farooq <mdanifarooq@gmail.com>
2023-09-21 02:10:00 -0700

    Update database.ts

--- cv builder/cv/database.ts
+++ cv builder/cv/database.ts
@@ -6,12 +6,7 @@ import { getStorage} from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCHF7RDPhORq-8ahsgphycfsLX3H-60RMc",
-  authDomain: "cvbuilder-74de4.firebaseapp.com",
-  projectId: "cvbuilder-74de4",
-  storageBucket: "cvbuilder-74de4.appspot.com",
-  messagingSenderId: "747413847781",
-  appId: "1:747413847781:web:c7a4e49d9ad22c730f5e0c"
+
 };
 
 // Initialize Firebase
